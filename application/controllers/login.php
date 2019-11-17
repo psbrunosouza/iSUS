b808252d61f9dbdd583353185c2c25db14838295
William Santos <smurf455@hotmail.com>
2019-11-17 18:05:25 -0300

    Update login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -3,9 +3,7 @@
     class Login extends CI_Controller{
 
         public function index(){
-            $this->load->view("templates/header");
             $this->load->view("pages/login");
-            $this->load->view("templates/footer");
         }
 
     }
