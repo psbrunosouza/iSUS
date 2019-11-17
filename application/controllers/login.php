aecb60c6da02ba021d1227516d13733640d46b00
Bruno <psbrunosouza@gmail.com>
2019-11-17 18:45:38 -0300

    update

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -3,9 +3,8 @@
     class Login extends CI_Controller{
 
         public function index(){
-            $this->load->view("templates/header");
+            $this->load->view("templates/head");
             $this->load->view("pages/login");
-            $this->load->view("templates/footer");
         }
 
     }
