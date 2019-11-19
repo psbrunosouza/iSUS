277bb70af34d4ba5a843714f41d8335c27bb0519
Bruno <psbrunosouza@gmail.com>
2019-11-18 22:17:35 -0300

    u

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,5 +1,31 @@
-<div class="container bg-secondary" style="height: 250px;">
-    <div class="mx-auto bg-primary" style="height: 350px; width:350px;">
-        
+<div class="container">
+    <div class="mt-5"></div>
+    <div class="mt-5"></div>
+
+    <div class="card border-primary mb-3 mx-auto" style="max-width: 22rem;">
+      <div class="card-header">SUSA - Login</div>
+      <div class="card-body text-dark">
+        <p class="card-text">Entre com suas credencias de acesso ou siga para a
+            página de registro para novos usuários.</p>
+        <form>
+            <div class="form-group">
+                <label for="cpfInput">CPF: </label>
+                <input type="text" class="form-control" id="cpfInput" aria-describedby="cpfHelp" placeholder="Digite seu CPF">
+            </div>
+            <div class="form-group">
+                <label for="inputSenha">Senha: </label>
+                <input type="password" class="form-control" id="inputSenha" placeholder="Senha">
+            </div>
+            <div>
+                <button type="button" class="btn btn-link"><a href="#rs">Registre-se</a></button>
+            </div>
+            <div>
+                <button type="button" class="btn btn-link"><a href="#ra">Recuperar acesso</a></button>
+            </div>
+            <div>
+                <button type="button" class="btn btn-link"><a href="#en">Entrar</a></button>
+            </div>
+        </form>
+      </div>
     </div>
 </div>
