fd05827d087a044c0ebc0dd5e3b3845ba6c103de
Bruno <psbrunosouza@gmail.com>
2019-11-19 20:33:07 -0300

    update

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -7,15 +7,25 @@
       <div class="card-body text-dark">
         <p class="card-text">Entre com suas credencias de acesso ou siga para a
             página de registro para novos usuários.</p>
-        <form>
-            <div class="form-group">
-                <label for="cpfInput">CPF: </label>
-                <input type="text" class="form-control" id="cpfInput" aria-describedby="cpfHelp" placeholder="Digite seu CPF">
-            </div>
-            <div class="form-group">
-                <label for="inputSenha">Senha: </label>
-                <input type="password" class="form-control" id="inputSenha" placeholder="Senha">
-            </div>
+            <?php
+            echo form_open("login/autenticar");
+                echo form_label ($label_text = "CPF: ", $id = "cpf");
+                echo form_input(array (
+                  "name" => "cpf",
+                  "id" =>  "cpf",
+                  "class" => "form-control",
+                  "maxlenght" => "11"
+                ));
+
+                echo form_label ($label_text = "Senha: ", $id = "senha");
+                echo form_input(array (
+                  "name" => "senha",
+                  "id" =>  "senha",
+                  "class" => "form-control",
+                  "maxlenght" => "24"
+                ));
+            echo form_close();
+             ?>
             <div>
                 <button type="button" class="btn btn-link"><a href="#rs">Registre-se</a></button>
             </div>
